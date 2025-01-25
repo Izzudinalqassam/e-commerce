a4da6dbed2b0dff7d80b881d8d3ca672af2bf35b
izzudinalqassam <alkasam987@gmail.com>
2025-01-25 17:53:36 +0700

    Admin Edit Brand and add param {id}

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ use App\Models\Brands;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Laravel\Facades\Image;
 use Illuminate\Support\Facades\Log; // Importing Log facade
 
@@ -28,33 +29,65 @@ class AdminController extends Controller
         return view('admin.brand-add');
     }
 
+    public function brand_edit($id)
+    {
+        $brand = Brands::find($id);
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request, $id)
+    {
+        $brand = Brands::find($id);
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug,' . $brand->id,
+            'image' => 'mimes:jpg,jpeg,png|max:2048',
+        ]);
+
+        $brand = Brands::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path('/uploads/brands') . '/' . $brand->image)) {
+                File::delete(public_path('/uploads/brands') . '/' . $brand->image);
+            }
+            $image = $request->file('image');
+            $file_extension = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+            $this->GenerateBrandThumbnailIsImage($image, $file_name);
+            $brand->image = $file_name;
+        }
+
+        $brand->save();
+        return redirect()->route('admin.brand')->with('status', 'Brand Update Successfully');
+    }
+
     public function brand_store(Request $request)
     {
         $request->validate([
             'name' => 'required',
             'slug' => 'required|unique:brands,slug',
-            'image' => 'mimes:jpg,jpeg,png',
+            'image' => 'mimes:jpg,jpeg,png | max:2048',
         ]);
         $brand = new Brands();
         $brand->name = $request->name;
         $brand->slug = Str::slug($request->name);
         $image = $request->file('image');
         $file_extension = $request->file('image')->extension();
-        $file_name =Carbon::now()->timestamp . '.' . $file_extension;
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
         $this->GenerateBrandThumbnailIsImage($image, $file_name);
         $brand->image = $file_name;
         $brand->save();
         return redirect()->route('admin.brand')->with('status', 'Brand Added Successfully');
-
     }
 
     public function GenerateBrandThumbnailIsImage($image, $imageName)
     {
         $destinationPath = public_path('/uploads/brands');  // upload path
         $img = Image::read($image->path());
-        $img->cover(124,124,"top");
-        $img->resize(124,124,function($constraint){
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constraint) {
             $constraint->aspectRatio();
-        })->save($destinationPath.'/'.$imageName);
+        })->save($destinationPath . '/' . $imageName);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,16 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 
 Auth::routes();
-Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
 
-Route::middleware(['auth'])->group(function(){
-    Route::get('/account-dashboard', [UserController::class,'index'])->name('user.index');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
 });
-Route::middleware(['auth',AuthAdmin::class])->group(function(){
-    Route::get('/admin', [AdminController::class,'index'])->name('admin.index');
-    Route::get('admin/brand', [AdminController::class,'brands'])->name('admin.brand');
-    Route::get('admin/brand/add', [AdminController::class,'add_brand'])->name('admin.brand.add');
-    Route::post('admin/brand/store', [AdminController::class,'brand_store'])->name('admin.brand.store');
+Route::middleware(['auth', AuthAdmin::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('admin/brand', [AdminController::class, 'brands'])->name('admin.brand');
+    Route::get('admin/brand/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
+    Route::post('admin/brand/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
+    Route::get('admin/brand/edit/{id}', [AdminController::class, 'brand_edit'])->name('admin.brand.edit');
+    Route::put('admin/brand/update/{id}', [AdminController::class, 'brand_update'])->name('admin.brand.update');
 });
-
